4b17f4ba43c60fb946b53d621adb583507a7c580
Patti85 <pbeier24@googlemail.com>
2021-02-11 22:36:38 +0100

    mail

--- backend/server.js
+++ backend/server.js
@@ -10,16 +10,18 @@ let Pidgeon = require('./models/pidgeon');
 
 app.use(cors());
 app.use(bodyParser.json());
+require('dotenv').config();
 
 mongoose.connect('mongodb://127.0.0.1:27017/pidgeons', { useNewUrlParser: true });
 const connection = mongoose.connection;
 
-connection.once('open', function() {
+connection.once('open', function () {
     console.log("MongoDB database connection established successfully");
 })
 
-pidgeonRoutes.route('/').get(function(req, res) {
-    Pidgeon.find(function(err, pidgeons) {
+pidgeonRoutes.route('/').get(function (req, res) {
+
+    Pidgeon.find(function (err, pidgeons) {
         if (err) {
             console.log(err);
         } else {
@@ -28,39 +30,62 @@ pidgeonRoutes.route('/').get(function(req, res) {
     });
 });
 
-pidgeonRoutes.route('/:id').get(function(req, res) {
+pidgeonRoutes.route('/:id').get(function (req, res) {
     let id = req.params.id;
-    Pidgeon.findById(id, function(err, pidgeon) {
+    Pidgeon.findById(id, function (err, pidgeon) {
         res.json(pidgeon);
     });
 });
 
-pidgeonRoutes.route('/update/:id').post(function(req, res) {
-    Pidgeon.findById(req.params.id, function(err, pidgeon) {
+pidgeonRoutes.route('/update/:id').post(function (req, res) {
+    Pidgeon.findById(req.params.id, function (err, pidgeon) {
         if (!pidgeon)
             res.status(404).send("data is not found");
         else
             pidgeon.description = req.body.description;
-            pidgeon.town = req.body.town;
-            pidgeon.latitude = req.body.latitude;
-            pidgeon.longitude = req.body.longitude;
-            pidgeon.responsible_person_registered = req.body.responsible_person_registered;
-            
+        pidgeon.town = req.body.town;
+        pidgeon.latitude = req.body.latitude;
+        pidgeon.longitude = req.body.longitude;
+        pidgeon.responsible_person_registered = req.body.responsible_person_registered;
 
-            pidgeon.save().then(pidgeon => {
-                res.json('Pidgeon updated!');
-            })
+
+        pidgeon.save().then(pidgeon => {
+            res.json('Pidgeon updated!');
+        })
             .catch(err => {
                 res.status(400).send("Update not possible");
             });
     });
 });
 
-pidgeonRoutes.route('/add').post(function(req, res) {
+pidgeonRoutes.route('/add').post(function (req, res) {
     let pidgeon = new Pidgeon(req.body);
+    let nodemailer = require('nodemailer');
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASS
+        }
+    });
+    let mailOptions = {
+        from: process.env.EMAIL,
+        to: process.env.EMAIL,
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+    };
+
     pidgeon.save()
         .then(pidgeon => {
-            res.status(200).json({'pidgeon': 'pidgeon added successfully'});
+            res.status(200).json({ 'pidgeon': 'pidgeon added successfully' });
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
         })
         .catch(err => {
             res.status(400).send('adding new pidgeon failed');
@@ -69,6 +94,6 @@ pidgeonRoutes.route('/add').post(function(req, res) {
 
 app.use('/pidgeons', pidgeonRoutes);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("Server is running on Port: " + PORT);
 });
