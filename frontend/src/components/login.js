b0c3072f342c323f80678801726d2c3a0987973e
Patti85 <pbeier24@googlemail.com>
2021-02-23 23:19:27 +0100

    login comp

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,9 +1,74 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+      
+        this.state = {
+            password: '',
+            email: ''
+        }
+    }
+    onChangeEmail(e) {
+        this.setState({
+            password: e.target.value
+        });
+    }
+    onChangePassword(e) {
+        this.setState({
+            email: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const submittedUser = {
+            email: this.state.email,
+            password: this.state.password,
+        };
+
+        axios.post('http://localhost:4000/login', submittedUser)
+            .then(res => console.log(res.data));
+
+        this.setState({
+            password: '',
+            email: ''
+        })
+    }
+
     render() {
         return (
-            <div>Hello</div>
+            <div>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>E-Mail</label>
+                        <input
+                            type="text"
+                            className="form-control"
+
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Passwort</label>
+                        <input
+                            type="text"
+                            className="form-control"
+
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Login" className="btn btn-primary" />
+                    </div>
+
+
+                </form>
+            </div>
         )
     }
 
