300db451c8ffd2c0bbdc3d72737af53373c4f707
Patti85 <pbeier24@googlemail.com>
2021-02-24 23:07:50 +0100

    login comp

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 export default class Login extends Component {
-
 
     constructor(props) {
         super(props);
 
         this.onChangeEmail = this.onChangeEmail.bind(this);
         this.onChangePassword = this.onChangePassword.bind(this);
-      
+        this.onSubmit = this.onSubmit.bind(this);
         this.state = {
             password: '',
             email: ''
@@ -16,12 +15,12 @@ export default class Login extends Component {
     }
     onChangeEmail(e) {
         this.setState({
-            password: e.target.value
+            email: e.target.value
         });
     }
     onChangePassword(e) {
         this.setState({
-            email: e.target.value
+            password: e.target.value
         });
     }
 
@@ -49,9 +48,10 @@ export default class Login extends Component {
                     <div className="form-group">
                         <label>E-Mail</label>
                         <input
-                            type="text"
+                            type="email"
                             className="form-control"
-
+                            value={this.state.email}
+                            onChange={this.onChangeEmail}
                         />
                     </div>
                     <div className="form-group">
@@ -59,11 +59,15 @@ export default class Login extends Component {
                         <input
                             type="text"
                             className="form-control"
-
+                            value={this.state.password}
+                            onChange={this.onChangePassword}
                         />
                     </div>
                     <div className="form-group">
-                        <input type="submit" value="Login" className="btn btn-primary" />
+                        <input
+                            type="submit"
+                            value="Login"
+                            className="btn btn-primary" />
                     </div>
 
 
